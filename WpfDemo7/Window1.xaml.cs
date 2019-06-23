c659e1c0c2f8d9c1df56a33d8941a4a37033fe9b
xiaozixiang <973870677@qq.com>
2019-06-23 21:34:12 +0800

    添加故事版demo

---
+++ WpfDemo7/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfDemo7
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Storyboard_CurrentTimeInvalidated(object sender, EventArgs e)
+        {
+            Clock clock = (Clock)sender;
+            if (clock.CurrentProgress == null)
+                this.progressbar.Value = 0;
+            else
+            this.progressbar.Value = (double)clock.CurrentProgress;
+        }
+
+        
+    }
+}
